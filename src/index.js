974c3b23ab0b7ea4e0a6f963b428a4dd9fc33806
maxim-savchuk <maxsavchuk1105@gmail.com>
2021-06-29 22:12:31 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './sass/main.scss';
+
+import cards from './menu.json';
+import itemsTemlate from './templates/cards.hbs';
+
+const menuRef = document.querySelector('.js-menu');
+
+const markup = itemsTemlate(cards);
+menuRef.insertAdjacentHTML('beforeend', markup);
+
+const switchThemeBtn = document.querySelector('#theme-switch-toggle');
+switchThemeBtn.addEventListener('click', onThemeBtnClick);
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
